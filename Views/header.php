4b6eb9d67d07bb8dc120e2fd5745a4d4ff034110
MartiBlanco <blancomarti87@gmail.com>
2021-10-15 20:59:19 -0300

    Acomodando

--- Views/header.php
+++ Views/header.php
@@ -8,11 +8,12 @@
      <!-- Bootstrap CSS -->
      <link rel="stylesheet" type="text/css" href="<?php echo CSS_PATH ?>bootstrap.min.css">
      <link rel="stylesheet" type="text/css" href="<?php echo CSS_PATH ?>Style.css">
+     <link rel="stylesheet" type="text/css" href="<?php echo CSS_PATH ?>fa/css/all.min.css">
+     
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+     
      
 
      <title>Bolsa de Trabajo</title>
 </head>
-
-
-
 <body>
