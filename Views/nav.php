612de53826752ead32b5ec3ade98cda9c023f46c
MartiBlanco <blancomarti87@gmail.com>
2021-10-17 01:04:39 -0300

    Error premaster

--- Views/nav.php
+++ Views/nav.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg  navbar-dark bg-dark">
      <span class="navbar-text">
-
+     <img src="Views/Images/utn.jpg" alt="" class="brand">
      </span>
 
      <ul class="navbar-nav ml-auto">
