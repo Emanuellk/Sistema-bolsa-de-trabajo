d21700674470b752fcfdc73804ea6fd44d77dca2
Emanuellk <emanuel.maggiolk@gmail.com>
2021-10-22 21:02:34 -0300

    Validar empresa al cargar y modificando vistas

--- Views/company-add.php
+++ Views/company-add.php
@@ -6,24 +6,24 @@
             <div class="container">
                 <br>
                 <h2 class="mb-4">Agregar empresa</h2>
-                <form action="<?php echo FRONT_ROOT ?>Company/Add" method="post" class="bg-light-alpha p-5">
+                <form action="<?php echo FRONT_ROOT ?>Company/Add" method="POST" class="bg-light-alpha p-5">
                         <div class="row">                         
                             <div class="col-lg-4">
                                 <div class="form-group">
                                     <label for="">Nombre de la Empresa</label>
-                                    <input type="text" name="nameCompany" value="" class="form-control">
+                                    <input type="text" name="nameCompany" value="" class="form-control" required>
                                 </div>
                             </div>
                             <div class="col-lg-4">
                                 <div class="form-group">
                                     <label for="">Email</label>
-                                    <input type="email" name="email" value="" class="form-control">
+                                    <input type="email" name="email" value="" class="form-control" required>
                                 </div>
                             </div>
                             <div class="col-lg-4">
                                 <div class="form-group">
                                     <label for="">Fecha de creación</label>
-                                    <input type="date" name="createDate" value="" class="form-control">
+                                    <input type="date" name="createDate" value="" class="form-control" required>
                                 </div>
                             </div>
                         </div>
