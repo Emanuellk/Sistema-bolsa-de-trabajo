05d537bf51a54934e63d91a7c657c639952de422
MartiBlanco <blancomarti87@gmail.com>
2021-10-27 21:03:31 -0300

    Commit arreglo config

--- Config/Config.php
+++ Config/Config.php
@@ -1,5 +1,4 @@
 <?php namespace Config;
-<<<<<<< HEAD
 
 define("ROOT", dirname(__DIR__) . "/");
 //Path to your project's root folder
@@ -12,18 +11,6 @@ define("DB_NAME", "Bolsa-de-trabajo");
 define("DB_USER", "root");
 define("DB_PASS", "");
 
-=======
-    define("ROOT", dirname(__DIR__) . "/");
-    //Path to your project's root folder
-    define("FRONT_ROOT", "/TP_LabIV/");
-    define("VIEWS_PATH", "Views/");
-    define("CSS_PATH", FRONT_ROOT.VIEWS_PATH . "css/");
-    define("JS_PATH", FRONT_ROOT.VIEWS_PATH . "js/");
-    define("DB_HOST", "localhost");
-    define("DB_NAME", "Bolsa-de-trabajo");
-    define("DB_USER", "root");
-    define("DB_PASS", "");
->>>>>>> 61fb34f04564cd415585aaea76f2daa95409bfbe
 ?>
 
 
