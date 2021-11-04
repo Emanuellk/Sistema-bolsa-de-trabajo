df0a9ff5c22e8022440c0a6f5038c53fabcbb790
MolinaMartin <martinmolina0@hotmail.com>
2021-11-03 21:32:42 -0300

    filtro

--- Views/offer-view.php
+++ Views/offer-view.php
@@ -8,22 +8,17 @@ require_once('nav.php');
         <br>
                
                <h2 class="mb-4">Listado de Ofertas laborales</h2>
-               <div class="mb-4">
-               <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAVFJREFUSEvVleExBEEQRt9FQAoiQATIgAgQASJABIgAESACZEAEyIAIqKd6qub2Zmen7N4P/eu2dqdf99dfz81YcsyWnJ8+wCpwBOwCG1HEC3APXAGfrYWVAAfABSCkFCY/AW5aIF2Aya/j4ANwCTzF8zZwBmzF8150VOXkACt+i8oPKxUKOQ2Z1obkygHpoJWrfS3syk7Oo6veb3OAQ1wHdjJZ+g4q1yPgmc1aJTngOz5stW7T92MBXxW3/db7V4mc0R3wDChX0wzSkF0mLdgyZPdBKzcBtOk7sBLO0CGlSIV8xJZXt7o70NS6ibWiyZTBUAr9nyQZtGh3Bqlat9m27aQUDtb3wrSpv2/7NKpddsexcO6G8RpXgwmVJe2N77yX3P6FaPV86Wx+b/VCxgBS0v2MvtDJWMAgZApAFzJ3E08FSBCtPfdHNCWg6NT/D/gBeOVFGZeTouUAAAAASUVORK5CYII="/>                      
-               <input type="text" name="search" id="search" placeholder="Buscar..." class="src" autocomplete="off">
-               </div>
-               <div  style="background-color:chocolate;" class="table-responsive ">
-               <table class="table bg-light-alpha table-primary">
+              
+               <div   class="table-responsive ">
+               <table class="table bg-light-alpha table-primary" id="example" >
                     <thead class="table-dark" style="white-space:nowrap;  text-align:center  ">
-                    <tr>
-                         <th scope="row">Título</th>
-                         <th scope="row">Empresa</th>
-                         <th scope="row">Carrera</th>
-                         <th scope="row">Empleo</th>                         
-                         <th scope="row">Carga Horaria</th>                                                 
-                         <th scope="row">Salario</th>
-                         <th scope="row">Ver/Postular</th>
-                    </tr>
+                         <th scope="col">Título</th>
+                         <th scope="col">Empresa</th>
+                         <th scope="col">Carrera</th>
+                         <th scope="col">Empleo</th>                         
+                         <th scope="col">Carga Horaria</th>                                                 
+                         <th scope="col">Salario</th>
+                         <th scope="col">Ver/Postular</th>
                     </thead>
                     <tbody>
 
@@ -34,12 +29,12 @@ require_once('nav.php');
                                    ?>
                                         <tr style="white-space:nowrap;">
 
-                                             <td><li class="list-group-item list-group-item-info"><?php echo $offer->getTitle() ?></li></td>
-                                             <td><li class="list-group-item list-group-item-info"><?php echo $offer->getNameCompany() ?> </li></td>
-                                             <td><li class="list-group-item list-group-item-info"><?php echo $offer->getCareerDescription() ?> </li></td>
-                                             <td><li class="list-group-item list-group-item-info"><?php echo $offer->getPositionDescription() ?></li></td>
-                                             <td><li class="list-group-item list-group-item-info"><?php echo $offer->getWorkLoad() ?></li></td>                              
-                                             <td><li class="list-group-item list-group-item-info"><?php echo $offer->getSalary() ?></li></td>
+                                             <td scope="row"><li class="list-group-item list-group-item-info"><?php echo $offer->getTitle() ?></li></td>
+                                             <td scope="row"><li class="list-group-item list-group-item-info"><?php echo $offer->getNameCompany() ?> </li></td>
+                                             <td scope="row"><li class="list-group-item list-group-item-info"><?php echo $offer->getCareerDescription() ?> </li></td>
+                                             <td scope="row"><li class="list-group-item list-group-item-info"><?php echo $offer->getPositionDescription() ?></li></td>
+                                             <td scope="row"><li class="list-group-item list-group-item-info"><?php echo $offer->getWorkLoad() ?></li></td>                              
+                                             <td scope="row"><li class="list-group-item list-group-item-info"><?php echo $offer->getSalary() ?></li></td>
                                              <td>                     
                                              
                                                   <button  class = "btn btn-success" type="button"  style="background-color:darkturquoise;" data-bs-toggle="modal" data-bs-target="#See<?= $offer->getId()."s"?> " >
@@ -146,30 +141,61 @@ require_once('nav.php');
 
                                          </tr>
                                    <?php
+                                  
                               }
                          ?>
+
                          <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
                     </tbody>
                </table>
+              
                </div>
           </div>
      </section>
 </div>
 
-<script>
-     const searchInput = document.getElementById("search");
-     const rows = document.querySelectorAll("tbody tr");
-     console.log(rows);
-     searchInput.addEventListener("keyup", function (event) {
-     const q = event.target.value.toLowerCase();
-     rows.forEach((row) => {
-          row.querySelector("td").textContent.toLowerCase().startsWith(q)
-          ? (row.style.display = "table-row")
-          : (row.style.display = "none");
-          });
-     });
-</script>
-        </div>
-    </section>
 
-</div>
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+<script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/fixedheader/3.1.6/js/dataTables.fixedHeader.min.js"></script> 
+
+<script>
+
+
+$(document).ready(function(){
+    var table = $('#example').DataTable({
+       orderCellsTop: true,
+       fixedHeader: true, 
+       paging:   false,       
+       info:     false,
+
+      
+       
+    });
+
+    //Creamos una fila en el head de la tabla y lo clonamos para cada columna
+    
+   
+
+    $('#example thead tr:eq(1) th').each( function (i) {
+        var title = $(this).text(); //es el nombre de la columna
+        $(this).html( '<input type="text" placeholder="Search...'+title+'" />' );
+ 
+        $( 'input', this ).on( 'keyup change', function () {
+            if ( table.column(i).search() !== this.value ) {
+                table
+                    .column(i)
+                    .search( this.value )
+                    .draw();
+                    
+            }
+        } );
+    } );   
+});
+
+
+ 
+
+</script>
