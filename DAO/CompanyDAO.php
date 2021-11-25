41591b89ca03424c2edb3b7ddc7c28c6369caae0
Emanuellk <emanuel.maggiolk@gmail.com>
2021-11-24 21:14:15 -0300

    ...

--- DAO/CompanyDAO.php
+++ DAO/CompanyDAO.php
@@ -82,7 +82,26 @@
                 } 
         }
         
+        public function SearchNameCompany($nameCompany) {
 
+            try{
+                $query = "SELECT * FROM `".$this->tableName."` WHERE nameCompany='$nameCompany'";
+                $this->connection = Connection::GetInstance();
+                $resultSet = $this->connection->Execute($query);
+
+                $CompanyAux = NULL;
+                
+                if(!empty($resultSet[0]))
+                {        
+                    $CompanyAux = new Company($resultSet[0]['id'],$resultSet[0]['nameCompany'],$resultSet[0]['email'],$resultSet[0]['createDate'],$resultSet[0]['description']);       
+                }
+                
+                return $CompanyAux;
+
+            }catch(Exception $ex){
+                throw $ex;
+            }
+        }
 
         public function SearchById($id) {
 
